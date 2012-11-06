1190f6d6aa9c779c82abe7754cc45176935245b9
muraoak-y <muraoka-y@klab.com>
2012-11-06 20:56:58 +0900

    config.phpのMyInputFilterスペルミス修正

--- fuel/app/logs/2012/10/31.php
+++
@@ -1,7 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Info - 2012-10-31 14:56:00 --> Fuel\Core\Request::__construct - Creating a new Request with URI = "fuel/app/classes/model/status"
-Info - 2012-10-31 14:56:00 --> Fuel\Core\Request::execute - Called
-Info - 2012-10-31 14:56:00 --> Fuel\Core\Request::execute - Setting main Request
-Info - 2012-10-31 14:56:00 --> Fuel\Core\Request::__construct - Creating a new Request with URI = "welcome/404"
-Info - 2012-10-31 14:56:00 --> Fuel\Core\Request::execute - Called
